299e21082e206c21ee3a75297aa2882de2a6cb21
matheus baron <=>
2022-02-07 22:14:11 -0300

    Feat: Models context created

--- src/components/Model/ModelContext.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-interface CarModel {}
-
-interface ModelsContext {
-  wrapperRef: React.RefObject<HTMLElement>
-}
-
-export default React.createContext<ModelsContext>({} as ModelsContext)
