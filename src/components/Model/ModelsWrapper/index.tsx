efd55a50ead49171d3c16b7b654b0c2bbb25acde
= <=>
2022-02-08 23:34:11 -0300

    Feat: Overlay styles created

--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback, useRef, useState } from 'react';
+import ModelOverlay from '../ModelOverlay';
 import ModelsContext, { CarModel } from '../ModelsContext';
 
-import { Container, OverlaysRoot, ModelOverlay } from './styles'
+import { Container, OverlaysRoot } from './styles'
 
 const ModelWrapper: React.FC = ({ children }) => {
   const wrapperRef = useRef<HTMLDivElement>(null)
